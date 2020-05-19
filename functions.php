d77f5af07741adc0a4ec2c468e54dd8537bd9748
frozendiary <ra1nbowmist@yahoo.com>
2020-05-19 22:26:51 +0600

    Update functions.php
    Minor Fixes

--- functions.php
+++ functions.php
@@ -3,12 +3,3 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-    /* Add JFIF image file type for uploads */
-
-    add_filter( ‘upload_mimes’, ‘custom_myme_types’, 1, 1 );
-    function custom_myme_types( $mime_types ) {
-    $mime_types[‘jfif’] = ‘image/jfif+xml’; // Adding .jfif extension
-
-    return $mime_types;
-    }
