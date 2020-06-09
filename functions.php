f06838bbf07e89b56123590c31de46829a2ce1b4
frozendiary <ra1nbowmist@yahoo.com>
2020-06-10 03:56:25 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,14 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+// Override mime rules
+function fd_mime_rules( $fd_mimes ) {
+		$fd_mimes['svg'] = 'image/svg+xml';
+		$fd_mimes['otf'] = 'font/otf';
+		$fd_mimes['ttf'] = 'font/ttf';
+  
+	return $fd_mimes;
+}
+
+add_filter( 'upload_mimes', 'fd_mime_rules' );
